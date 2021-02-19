614a37cc218b1cf0f61b6bd6278d828f0136fa79
luisvinicius09 <luisvinicius0906@gmail.com>
2021-02-19 18:31:42 -0300

    Fix issues raised by TSE

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,8 @@
 module PostsHelper
+  def useless_helper(var)
+    if var
+      render inline: ("<div><h2>Create your post!</h2><%= link_to 'New Post', new_post_path, class: 'button' %></div>").html_safe
+    end
+  end
 end
+
