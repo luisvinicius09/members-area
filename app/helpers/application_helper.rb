0d932ee9193cd906add2a212292b7e0665c75437
luisvinicius09 <luisvinicius0906@gmail.com>
2021-02-19 19:37:57 -0300

    Fix issues raised by TSE

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,22 @@ module ApplicationHelper
   def alert_notification
     render inline: '<div class="notification"><p class="alert"><%= alert %></p></div>'.html_safe if flash[:alert]
   end
+
+  def buttons(var)
+    if var
+      render inline: '<p class="control">
+      <%= link_to "Logout", destroy_user_session_path, method: :delete, class: "button" %>
+      </p>'.html_safe
+    else
+      render inline: '<div style="display: flex">
+      <p class="control">
+        <%= link_to "Sign In", new_user_session_path, class: "button" %>
+      </p>
+      <p class="control">
+        <%= link_to "Sign Up", new_user_registration_path, class: "button" %>
+      </p>
+      </div>'.html_safe
+    end 
+  end
+
 end
---
+++ app/helpers/posts_helper.rb
@@ -0,0 +1,14 @@
+module PostsHelper
+  def useless_helper(var)
+    if var
+      render inline: "<div><h2>Create your post!</h2>
+      <%= link_to 'New Post', new_post_path, class: 'button' %>
+      </div>".html_safe
+    end
+  end
+  def second_useless_helper(var, var2)
+    if var
+      render inline: '<p>' + var2.user.name + '</p>'.html_safe
+    end
+  end
+end
