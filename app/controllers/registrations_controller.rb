6992c50cd82ba4716fd679e89600251f38fd14b3
luisvinicius09 <luisvinicius0906@gmail.com>
2021-02-19 12:13:08 -0300

    Add linters

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,5 +4,4 @@ class RegistrationsController < Devise::RegistrationsController
   def sign_up_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
-
 end
