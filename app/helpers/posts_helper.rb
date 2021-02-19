ed140dde5e6c1be23666532f9a971ccd36610c06
luisvinicius09 <luisvinicius0906@gmail.com>
2021-02-19 18:35:58 -0300

    Fix linters

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,7 +1,7 @@
 module PostsHelper
   def useless_helper(var)
-    if var
-      render inline: "<div><h2>Create your post!</h2><%= link_to 'New Post', new_post_path, class: 'button' %></div>".html_safe
-    end
+    render inline: "<div><h2>Create your post!</h2>
+    <%= link_to 'New Post', new_post_path, class: 'button' %>
+    </div>".html_safe if var
   end
 end
