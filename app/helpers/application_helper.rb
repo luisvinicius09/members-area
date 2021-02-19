1e0c681b6714c223661af9f3883acea12d0fce50
luisvinicius09 <luisvinicius0906@gmail.com>
2021-02-19 18:33:41 -0300

    Fix linters

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
   def notice_notification
-    render inline: ('<div class="notification"><p class="notice"><%= notice %></p></div>').html_safe if flash[:notice]
+    render inline: '<div class="notification"><p class="notice"><%= notice %></p></div>'.html_safe if flash[:notice]
   end
 
   def alert_notification
-    render inline: ('<div class="notification"><p class="alert"><%= alert %></p></div>').html_safe if flash[:alert]
+    render inline: '<div class="notification"><p class="alert"><%= alert %></p></div>'.html_safe if flash[:alert]
   end
 end
--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,7 +1,7 @@
 module PostsHelper
   def useless_helper(var)
     if var
-      render inline: ("<div><h2>Create your post!</h2><%= link_to 'New Post', new_post_path, class: 'button' %></div>").html_safe
+      render inline: "<div><h2>Create your post!</h2><%= link_to 'New Post', new_post_path, class: 'button' %></div>".html_safe
     end
   end
 end
